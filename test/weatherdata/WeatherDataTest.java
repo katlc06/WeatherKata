b4bae8629c307724e3b45ade09156720100cf5d9
katlc06 <katcheng60@gmail.com>
2024-10-27 02:25:25 -0400

    [Styling] Adding javadoc to the test class

--- test/weatherdata/WeatherDataTest.java
+++ test/weatherdata/WeatherDataTest.java
@@ -5,9 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.json.JSONObject;
 
-
+/**
+ * Tests the WeatherData Class
+ * @author Kathryn Cheng
+ */
 class WeatherDataTest {
-
+	/**
+	 * Tests the WeatherData class with a valid city.
+	 */
 	@Test
     public void testGetMockWeatherData_ValidCity() {
         JSONObject result = WeatherData.getMockWeatherData("London");
@@ -15,7 +20,9 @@ class WeatherDataTest {
         assertEquals("London", result.getString("name"));
         assertEquals(280.32, result.getJSONObject("main").getDouble("temp"));
     }
-
+	/**
+	 * Tests the WeatherData class with an invalid city.
+	 */
     @Test
     public void testGetMockWeatherData_InvalidCity() {
         JSONObject result = WeatherData.getMockWeatherData("InvalidCityName");
